8899bff94d1a2dc4682811a750e0273935c0eb31
Peter Aron Zentai <peter.zentai@avangrade.com>
2013-03-31 18:59:27 +0200

    refresh

---
+++ Pointer/Server/hl.js
@@ -0,0 +1,23 @@
+﻿require('odata-server');
+var connect = require('connect');
+var app = connect();
+
+
+var NWCategory = $data.Entity.extend('NorthwindModel.Category', {
+    CategoryID: { key: true, type: 'id', nullable: false, computed: true },
+    CategoryName: { type: 'string', nullable: false, required: true, maxLength: 15 }
+});
+
+
+var NWModel = $data.EntityContext.extend('NorthwindModel', {
+    Categories: { type: $data.EntitySet, elementType: NWCategory }
+});
+
+app.use("/northwind", $data.ODataServer({
+    type: NWModel,
+    CORS: true
+}));
+
+app.listen(12345);
+
+
