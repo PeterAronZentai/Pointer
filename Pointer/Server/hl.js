06fb779f7d98384c8ddd966c64c419fd34825f1b
Peter Aron Zentai <peter.zentai@jaystack.com>
2013-04-04 19:55:36 +0200

    reverse geo, tooltips and a helluva lot more

--- Pointer/Server/hl.js
+++ Pointer/Server/hl.js
@@ -10,11 +10,59 @@ var NWCategory = $data.Entity.extend('NorthwindModel.Category', {
 
 
 var NWModel = $data.EntityContext.extend('NorthwindModel', {
-    Categories: { type: $data.EntitySet, elementType: NWCategory },
-    getGeo: function (p) {
+    
+
+    reverse: function(p) {
         ///<param name="p" type="Object" />
         ///<returns type="Object" />
+        // http://dev.virtualearth.net/services/v1/geocodeservice/geocodeservice.asmx/ReverseGeocode?latitude=47.47189414358602&longitude=19.007935523986816&key=Anqm0F_JjIZvT0P3abS6KONpaBaKuTnITRrnYuiJCE0WOhH6ZbE4DzeT6brvKVR5&culture=%22en-us%22&format=json
+        console.log("reverse");
+        console.log("params:", p);
 
+        var result = '';
+
+        
+        return function (ok, fail) {
+            var http = require('http');
+            var resFn = function (res) {
+                res.on('data', function (d) {
+                    result += d.toString('utf-8');
+                    //console.log('response', d);
+                });
+                res.on('end', function () {
+                    var res = eval("(" + result + ")");
+                    console.log(res);
+                    //console.log('done:', res);
+                    ok(res);
+                    //console.log('end');
+                });
+            };
+
+            var errorFn = function (err) {
+                console.log('error', err);
+            };
+
+            var path = '/services/v1/geocodeservice/geocodeservice.asmx/ReverseGeocode?latitude=' + p.lat + '&longitude=' + p.lng + '&key=AmIrj9J3VEqVnTeA5WHadVN89sO5dvrf9blR6z2HX8npuRMyJYzzXhAR5t2S1ky-&culture=%22en-us%22&format=json';
+            console.log(path);
+            var req = http.request({
+                host: 'dev.virtualearth.net',
+                port: 80,
+                path: path,
+                method: 'GET',
+            }, resFn);
+            req.on('error', errorFn);
+            req.end();
+
+        };
+    },
+    getGeo: function getGeo(p, n) {
+        ///<param name="p" type="Object" />
+        ///<param name="n" type="string" />
+        ///<returns type="Object" />
+        console.log("getGeo");
+        console.log("params", p, n);
+        console.log(p);
+        console.log(n);
 
         var result = '';
 
@@ -27,7 +75,7 @@ var NWModel = $data.EntityContext.extend('NorthwindModel', {
                 });
                 res.on('end', function () {
                     var res = JSON.parse(result);
-                    console.log('done:', res);
+                    console.log('done:');
                     ok(res);
                     //console.log('end');
                 });
@@ -38,7 +86,7 @@ var NWModel = $data.EntityContext.extend('NorthwindModel', {
             };
 
 
-            var path = '/search?q=HYP3RL0C4LZZZ&limit=1000&ct_lat=' + p.lat + '&ct_lng=' + p.lng + '&radius=0.01';
+            var path = '/search?q=HYP3RL0C4LZZZ&limit=1000&ct_lat=' + p.lat + '&ct_lng=' + p.lng + '&radius=' + n;
             console.log(path);
             var req = http.request({
                 host: 'hyperlocal.redth.info',
@@ -49,7 +97,7 @@ var NWModel = $data.EntityContext.extend('NorthwindModel', {
             req.on('error', errorFn);
             req.end();
 
-        }
+        };
         //console.log(path);
         //var req = http.request({
         //    host: 'hyperlocal.redth.info',
@@ -62,11 +110,8 @@ var NWModel = $data.EntityContext.extend('NorthwindModel', {
     }
 });
 
-app.use("/northwind", $data.ODataServer({
-    type: NWModel,
-    CORS: true
-}));
+app.use("/hl", $data.ODataServer({ type: NWModel, CORS: true }));
 
-app.listen(12345,"192.168.10.101");
+app.listen(12345);
 
 
--- Pointer/main.js
+++ Pointer/main.js
@@ -5,16 +5,53 @@ function main() {
             item[i] = item.initData[i];
         }
     }
+    var redMarker = L.AwesomeMarkers.icon({
+        icon: 'spinner', color: 'red', spin: true
+    });
 
-    var ViewModel = function() {
+    var blueMarker = L.AwesomeMarkers.icon({
+        icon: 'cog', color: 'blue'
+    });
+
+    var ViewModel = function () {
         var self = this;
         self.poi = ko.observable();
-        self.removePoi = function() {
+
+
+        self.reversGeoStatus = ko.observable("Accquire address");
+        self.addressResults = ko.observable([]);
+
+
+        self.addNew = function () {
+            var o = self.poi().getEntity();
+            var arrs = self.addressResults();
+            if (arrs.length > 0) {
+                var addr = arrs[0].Address;
+                console.log(addr);
+                o.country = addr.CountryRegion;
+                o.province = addr.AdminDistrict;
+                o.postal = addr.PostalCode;
+                o.city = addr.Locality;
+                o.addr = addr.AddressLine;
+                o.mapObject().setIcon(blueMarker);
+                o.save();
+                $('#addNewPoint').foundation('reveal', 'close');
+                //console.dir(o);
+            }
+        }
+
+        self.cancelAddNew = function () {
+            var o = self.poi();
+            self.poi().getEntity().removeMapObject();
+            $('#addNewPoint').foundation('reveal', 'close');
+        }
+
+        self.removePoi = function () {
             self.poi().getEntity().removeMapObject();
             self.poi().getEntity().remove();
             $('#myModal').foundation('reveal', 'close');
         };
-        self.cancelEdit = function() {
+        self.cancelEdit = function () {
             self.poi().getEntity().refresh();
             $('#myModal').foundation('reveal', 'close');
         };
@@ -31,21 +68,66 @@ function main() {
     //var lmap = L.map('map').setView([47.4981, 19.04], 13);
 
 
-    lmap = new L.Map('map', { center: new L.LatLng(40.72121341440144, -74.00126159191132), maxZoom: 22, zoom: 12 });
-    //var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-    var bing = new L.BingLayer("Anqm0F_JjIZvT0P3abS6KONpaBaKuTnITRrnYuiJCE0WOhH6ZbE4DzeT6brvKVR5");
-    //var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/003d6e8d9af14e7582b462c10e572a1a/997/256/{z}/{x}/{y}.png');
-    lmap.addLayer(bing);
-    //lmap.addControl(new L.Control.Layers({ "Bing": bing, 'CloudMade': cloudmade, 'OSM': osm,  }, {}));
+    lmap = new L.Map('map', { center: new L.LatLng(40.72121341440144, -74.00126159191132), maxZoom: 20, zoom: 19 });
+    var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 22 });
+    var bing = new L.BingLayer("AmIrj9J3VEqVnTeA5WHadVN89sO5dvrf9blR6z2HX8npuRMyJYzzXhAR5t2S1ky-", { maxZoom: 22 });
+    var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/003d6e8d9af14e7582b462c10e572a1a/997/256/{z}/{x}/{y}.png', { maxZoom: 22 });
 
+    lmap.addLayer(bing);
+
+    //lmap.addControl(new L.Control.Layers({ "Bing": bing, 'CloudMade': cloudmade, 'OSM': osm }));
     //alert("getting position");
     navigator.geolocation.getCurrentPosition(function (o) {
         //console.dir(arguments);
-        lmap.setView([o.coords.latitude, o.coords.longitude], 15);
+        //lmap.setView([o.coords.latitude, o.coords.longitude], 19);
+        lmap.setView([40.72121341440144, -74.00126159191132], 19);
         //alert("position acquired");
         console.log("position:", o);
     })
+
     var lgroup = new L.LayerGroup().addTo(lmap);
+
+
+
+    var markerGroups = {
+        'My Pins': undefined,
+        'Food & Drink': L.AwesomeMarkers.icon({
+            icon: 'food', color: 'red', spin: false
+        }),
+        'Manufacturing & Wholesale Goods': L.AwesomeMarkers.icon({
+            icon: 'truck', color: 'yellow', spin: false
+        }),
+        'Public Place': L.AwesomeMarkers.icon({
+            icon: 'camera', color: 'purple', spin: false
+        }),
+        'Retail Goods': L.AwesomeMarkers.icon({
+            icon: 'shopping-cart', color: 'orange', spin: false
+        }),
+        'Services': L.AwesomeMarkers.icon({
+            icon: 'cog', color: 'darkgreen', spin: false
+        }),
+        'Transportation': L.AwesomeMarkers.icon({
+            icon: 'exchange', color: 'cadetblue', spin: false
+        })
+    }
+
+    var useClusters = false;
+
+    function getPinGroup() {
+        return useClusters ? new L.MarkerClusterGroup().addTo(lmap) : new L.LayerGroup().addTo(lmap);
+    }
+
+    var pinGroups = {
+        'My Pins': lgroup,
+        'Food & Drink': getPinGroup(),
+        'Manufacturing & Wholesale Goods': getPinGroup(),
+        'Public Place': getPinGroup(),
+        'Retail Goods': getPinGroup(),
+        'Services': getPinGroup(),
+        'Transportation': getPinGroup()
+    }
+
+    L.control.layers({ "Bing": bing, 'CloudMade': cloudmade, 'OSM': osm }, pinGroups, { position: 'topleft' }).addTo(lmap);
 
     //L.tileLayer('http://{s}.tile.cloudmade.com/003d6e8d9af14e7582b462c10e572a1a/997/256/{z}/{x}/{y}.png', {
     //    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
@@ -54,75 +136,155 @@ function main() {
 
 
 
-    function add2map(g) {
-        var onEachFeature = function(feature, layer) {
+    function add2map(g, icon) {
+        var onEachFeature = function (feature, layer) {
+            if (icon)
+                layer.setIcon(redMarker);
             layer.options.draggable = true;
-            layer.on('dragend', function(e) {
+            layer.on('dragend', function (e) {
                 g.latlon.coordinates = [e.target._latlng.lng, e.target._latlng.lat];
                 g.latlon = g.latlon;
                 resetProps(g);
                 g.save();
-
-
             });
-            layer.on('click', function() {
+            layer.on('click', function () {
                 v.poi(g.asKoObservable());
                 $('#myModal').foundation('reveal', 'open');
             });
         };
-        var x = L.geoJson(g.latlon, { onEachFeature: onEachFeature }); // .bindPopup(g.name);
-        lgroup.addLayer(x);
+
+        i = (icon ? { icon: icon, draggable: true } : { draggable: true });
+        var marker = L.marker([g.latlon.coordinates[1], g.latlon.coordinates[0]], i);
+
+        marker.on('dragend', function (e) {
+            g.latlon.coordinates = [e.target._latlng.lng, e.target._latlng.lat];
+            g.latlon = g.latlon;
+            resetProps(g);
+            g.save();
+        });
+
+        marker.on('click', function () {
+            v.poi(g.asKoObservable());
+            $('#myModal').foundation('reveal', 'open');
+        });
+       // var x = L.geoJson(g.latlon, { onEachFeature: onEachFeature }); // .bindPopup(g.name);
+        lgroup.addLayer(marker);
+
+        g.mapObject = function () {
+            return marker;
+        }
         g.removeMapObject = function () {
-            lgroup.removeLayer(x);
+            lgroup.removeLayer(marker);
         }
     }
 
-    var onTheMap = {};
-    
-    //$data.service("http://192.168.10.101:12345/hyperlocal").then(function (tp) {
-    //    var hl = tp();
+    processed = {};
+    cats = {};
+    types = {};
+    tags = {};
+    subcats = {};
+
+   var localService = null;
+
+    function getPointsAroundMe(service, radius) {
+        service.getGeo(lmap.getCenter(), radius).then(function (r) {
+            console.dir(r);
+            r.results.forEach(function (p) {
+                if (!processed[p.record_id]) {
+                    processed[p.record_id] = true;
+                    var layer = pinGroups[p.record.type];
+                    var icon = markerGroups[p.record.type];
+                    icon = (icon ? { icon: icon } : {});
+                    if (layer) {
+                        L.marker([p.record.lat, p.record.lon], icon)
+                         .bindPopup("<b>" + (p.record.name || "No name given") + "</b><br />" + (p.record.cat || '') + ","
+                               + (p.record.subcat) + "<br />" + (p.record.phone || "") + "<br/>" + p.record.addr)
+                         .addTo(layer);
+                    }
+
+                    //        onTheMap[p.record_id] = true;
+                    //stats
+                    //cats[p.record.cat] = cats[p.record.cat] || 0;
+                    //cats[p.record.cat]++;
+                    //types[p.record.type] = types[p.record.type] || 0;
+                    //types[p.record.type]++;
+                    //subcats[p.record.subcat] = subcats[p.record.subcat] || 0;
+                    //subcats[p.record.subcat]++;
+                }
+            });
+        });
+    }
+    //$data.service("http://dev-open.jaystack.net/a11d6738-0e23-4e04-957b-f14e149a9de8/1162e5ee-49ca-4afd-87be-4e17c491140b/api/mydatabase").then(function (tp) {
+    //$data.service("http://192.168.1.98:12345/hl").then(function (tp) {
+    //    var hl = localService = tp();
     //    hl.onReady().then(function () {
-    //        lmap.on("dragend", function () {
-    //            hl.getGeo(lmap.getCenter()).then(function (r) {
-    //                r.results.forEach(function (p) {
-    //                    if (!onTheMap[p.record_id]) {
-    //                        console.log("adding point", p);
-    //                        L.marker([p.record.lat, p.record.lon]).addTo(lmap);
-    //                        onTheMap[p.record_id] = true;
-    //                    }
-    //                });
-    //            });
-    //        });
+    //        lmap.on("dragend", function () { getPointsAroundMe(hl, '0.0008') });
+    //        getPointsAroundMe(hl, '0.0008');
     //    });
     //});
 
     $data.initService('http://dev-open.jaystack.net/a11d6738-0e23-4e04-957b-f14e149a9de8/1162e5ee-49ca-4afd-87be-4e17c491140b/api/mydatabase')
     .then(function (mydatabase, factory, type) {
 
+        localService = mydatabase;
+        var aroundMeTimer;
+
+        //lmap.on("movestart", function () {
+        //    window.clearTimeout(aroundMeTimer);
+        //    //console.log("dragstart");
+        //});
+        lmap.on("dragstart", function () {
+            window.clearTimeout(aroundMeTimer);
+            //console.log("dragstart");
+        });
+        lmap.on("dragend", function () {
+            aroundMeTimer = window.setTimeout(function () {
+                var r = lmap.getCenter().distanceTo(lmap.getBounds().getSouthWest()) / 150000;
+                r = r.toString();
+                getPointsAroundMe(mydatabase, r);
+            }, 800);
+            
+        });
+        var r = lmap.getCenter().distanceTo(lmap.getBounds().getSouthWest()) / 150000;
+        r = r.toString();
+        getPointsAroundMe(mydatabase, r);
+
         mydatabase
             .HyperLocal
-            .toArray(function(result) {
-                result.forEach(function(g) {
-                    add2map(g);
+            .toArray(function (result) {
+                result.forEach(function (g) {
+                    add2map(g, blueMarker);
                 });
             });
 
         var timer;
 
-        lmap.on('click', function(e) {
+        lmap.on('click', function (e) {
             window.clearTimeout(timer);
-            timer = window.setTimeout(function() {
+            timer = window.setTimeout(function () {
                 var g = new mydatabase.HyperLocal.elementType();
                 g.latlon = new $data.GeographyPoint(); // $data.Point(e.latlng);
                 g.latlon.coordinates = [e.latlng.lng, e.latlng.lat];
                 g.z = "HYP3RL0C4LZZZ";
-                g.save();
-                add2map(g);
-            }, 200);        
+                v.reversGeoStatus("Acquiring address...");
+                v.addressResults([]);
+                add2map(g, redMarker);
+                $('#addNewPoint').foundation('reveal', 'open');
+                v.poi(g.asKoObservable());
+                localService.reverse(e.latlng).then(function (r) {
+                    console.log(r.Results);
+                    if (r.Results.length > 1) {
+                        v.reversGeoStatus("Multiple addresses!");
+                    } else {
+                        v.reversGeoStatus("Address found");
+                    }
+                    v.addressResults(r.Results);
+                });
+            }, 200);
         });
-        lmap.on('dblclick', function(e) {
+        lmap.on('dblclick', function (e) {
             window.clearTimeout(timer);
         });
-     });
+    });
 };
 
