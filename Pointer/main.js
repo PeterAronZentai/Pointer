cbcce774dceb5625a217077284c0bd4252a1eaf4
Gabor Dolla <agdolla@gmail.com>
2013-04-11 13:33:20 +0200

    oauth2 auth

--- Pointer/main.js
+++ Pointer/main.js
@@ -436,4 +436,23 @@ function main() {
             }
         });
     });
+
+    hello.init({ 
+        //facebook: '439026716191124',
+        google: '449285537332-jkcc38anllj53up1frq2cjjockgpct5i.apps.googleusercontent.com'
+    });
+
+    hello.subscribe('auth.login', function(auth){
+	
+        // call user information, for the given network
+        hello.api( auth.network + '/me', function(r){
+            if(!r.id || !!document.getElementById(r.id) ){
+                return;
+            }
+            //var target = document.getElementById("profile_"+ auth.network );
+            //target.innerHTML = '<img src="'+ r.picture +'" /> Hey '+r.name;
+            console.log(r.name);
+        });
+    });
+    hello.login('google');
 };
